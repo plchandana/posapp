9112ad7baa40ff898cf2ac99d3110b168cf2bc5d
plchandana <115790886+plchandana@users.noreply.github.com>
2024-03-13 13:36:42 +0530

    my conection

--- src/api/rootAPI.js
+++ src/api/rootAPI.js
@@ -1,2 +1,2 @@
-export const rootAPI = `https://posapp-three.vercel.app`;
-// export const rootAPI = `http://localhost:3000`;
+export const rootAPI = `https://posapi-6xws42a5n-chandanas-projects-7410ea33.vercel.app/`;
+ //export const rootAPI = `http://localhost:3000`;
