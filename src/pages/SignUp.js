8e2c2f5479cbee3a8bd27f3a5cbb735d520766c3
plchandana <115790886+plchandana@users.noreply.github.com>
2024-03-14 16:13:58 +0530

    my thired

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,9 +1,9 @@
-import {userSaveAPI} from "../api/user/userAPI";
+import { userSaveAPI } from "../api/user/userAPI";
 import CustomButton from "../components/CustomButton";
 import CustomTextField from "../components/CustomTextField";
-import {useState} from "react";
+import { useState } from "react";
 import "../assets/theme/App.css"
-import {Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 
 
@@ -23,78 +23,81 @@ function SignUp() {
         setFormData({
             ...formData,
             [e.target.name]: e.target.value
-        });}
+        });
+    }
 
 
-        const signUpFunction = async () => {
-            try {
-                const res = await fetch(`${userSaveAPI}`, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Access-Control-Allow-Origin': '*',
-                    },
-                    body: JSON.stringify(formData)
-                })
-    
-                if (res.status === 200) {
-                    alert("User Register Success!")
-                    navigate('/sign-in', {replace: true})
-                } else {
-                    alert("User Register Fail!")
+    const signUpFunction = async () => {
+             try {
+                    const res = await fetch(`${userSaveAPI}`, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Access-Control-Allow-Origin': '*',
+                        },
+                        body: JSON.stringify(formData)
+                    })
+        console.log(
+            'res', res
+         );
+                    if (res.status === 200) {
+                        alert("User Register Success!")
+                        navigate('/sign-in', {replace: true})
+                    } else {
+                        alert("User Register Fail!")
+                    }
+                } catch (e) {
                 }
-            } catch (e) {
-            }
-        }
-    
-        return (
-            <div className="containor">
-                <div className="content">
-                    <h1>SIGN UP</h1>
-    
-                    <CustomTextField
-                        PLACE_HOLDER={"Username"}
-                        TYPE={"text"}
-                        NAME={"username"}
-                        VALUE={formData.username}
-                        ON_CHANGE={handleChange}
-                    />
-                    <br></br>
-                    <CustomTextField
-                        PLACE_HOLDER={"Email"}
-                        TYPE={"email"}
-                        NAME={"email"}
-                        VALUE={formData.email}
-                        ON_CHANGE={handleChange}
-                    />
-                    <br></br>
-                    <CustomTextField
-                        PLACE_HOLDER={"Mobile Number"}
-                        TYPE={"number"}
-                        NAME={"mobile"}
-                        VALUE={formData.mobile}
-                        ON_CHANGE={handleChange}
-                    />
-                    <br></br>
-    
-                    <CustomTextField
-                        PLACE_HOLDER={"Password"}
-                        TYPE={"password"}
-                        NAME={"password"}
-                        VALUE={formData.password}
-                        ON_CHANGE={handleChange}
-                    />
-    
-                    <div className={"showPasswordSection"}>
-                        <input type="checkbox"></input>
-                        <label>Show password</label>
-                    </div>
-    
-                    <CustomButton BTN_NAME={"SIGN UP"} ON_CLICK={signUpFunction}/>
-    
-                    <p>
-                        Already Have an account ? <Link sty to="/sign-in">Sign In</Link>
-                    </p></div>
+    }
+
+    return (
+        <div className="containor">
+            <div className="content">
+                <h1>SIGN UP</h1>
+
+                <CustomTextField
+                    PLACE_HOLDER={"Username"}
+                    TYPE={"text"}
+                    NAME={"username"}
+                    VALUE={formData.username}
+                    ON_CHANGE={handleChange}
+                />
+                <br></br>
+                <CustomTextField
+                    PLACE_HOLDER={"Email"}
+                    TYPE={"email"}
+                    NAME={"email"}
+                    VALUE={formData.email}
+                    ON_CHANGE={handleChange}
+                />
+                <br></br>
+                <CustomTextField
+                    PLACE_HOLDER={"Mobile Number"}
+                    TYPE={"number"}
+                    NAME={"mobile"}
+                    VALUE={formData.mobile}
+                    ON_CHANGE={handleChange}
+                />
+                <br></br>
+
+                <CustomTextField
+                    PLACE_HOLDER={"Password"}
+                    TYPE={"password"}
+                    NAME={"password"}
+                    VALUE={formData.password}
+                    ON_CHANGE={handleChange}
+                />
+
+                <div className={"showPasswordSection"}>
+                    <input type="checkbox"></input>
+                    <label>Show password</label>
+                </div>
+
+                <CustomButton BTN_NAME={"SIGN UP"} ON_CLICK={signUpFunction} />
+
+                <p>
+                    Already Have an account ? <Link sty to="/sign-in">Sign In</Link>
+                </p></div>
         </div>
     );
 }
